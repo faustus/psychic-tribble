5488b7d67b30e168606bf7b7c5c48064f04c7043
Wes Cothran <wes.cothran@gmail.com>
2013-12-02 10:58:06 -0600

    Update comparison_test.cpp

--- test/test_compare/comparison_test.cpp
+++ test/test_compare/comparison_test.cpp
@@ -1,4 +1,3 @@
-//
 int main(int argc, char **argv) {
 
    auto_diff<double,4> q[4],f[4];
@@ -18,25 +17,18 @@ int main(int argc, char **argv) {
       ubar = nx*q[1]/q[0]+ny*q[2]/q[0];
       u = q[1]/q[0];
       v = q[2]/q[0];
-      
       p = (gam - 1.0)*(q[3] - .5*(q[1]*q[1] + q[2]*q[2])/q[0]);
-
       cs = pow((gam*p)/q[0],.5)+pow((gam*p)/q[0],.5);
 
       f[0] = l*0.25*q[0]*cs*(ubar/cs+1.0)*(ubar/cs+1.0);
       f[1] = f[0]*((nx/gam)*(-1.0*ubar)+(nx/gam)*(2.0*cs)+u);
       f[2] = f[0]*((ny/gam)*(-1.0*ubar)+(ny/gam)*(2.0*cs)+v);
-      f[3] = f[0]*((-(gam-1.0)*ubar*ubar+2.0*(gam-1.0)*ubar*cs+2.0*cs*cs)
-            /(gam*gam-1.0)+0.5*(u*u)+0.5*(v*v));
-
-
-
+      f[3] = f[0]*((-(gam-1.0)*ubar*ubar+2.0*(gam-1.0)*ubar*cs+2.0*cs*cs)/(gam*gam-1.0)+0.5*(u*u)+0.5*(v*v));
    }
    for(int j=0; j <= 3; j++) {
       cout << "j   " << f[j].Value() << endl;
       for(int k=0; k <= 3; k++)
          cout << "j,k   " << f[j].Deriv(k) << endl;
    }
-
    return 0;
 }
