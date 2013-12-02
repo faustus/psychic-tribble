3d1d7b7b52c7a5c0795e29563f2933a89f4c8fcb
Wes Cothran <wes.cothran@gmail.com>
2013-12-02 10:57:10 -0600

    Update comparison_test.cpp
    Should output 
    j   0.373533
    j,k   0.136378
    j,k   0.0806219
    j,k   0.534104
    j,k   0.0779302
    j   0.544331
    j,k   -0.256844
    j,k   0.410951
    j,k   0.728296
    j,k   0.164278
    j   0.232243
    j,k   0.118799
    j,k   -0.190079
    j,k   0.55553
    j,k   0.200595
    j   0.770441
    j,k   -0.432248
    j,k   0.0752447
    j,k   1.20063
    j,k   0.633505

--- test/test_compare/comparison_test.cpp
+++ test/test_compare/comparison_test.cpp
@@ -1,1 +1,42 @@
 //
+int main(int argc, char **argv) {
+
+   auto_diff<double,4> q[4],f[4];
+   auto_diff<double,4> cs, ubar, u, v, p;
+   double nx = 0.25,ny = 0.75,gam = 1.4,l = 1.0;
+
+   q[0].set_val(1.0);
+   q[1].set_val(1.25);
+   q[2].set_val(0.0);
+   q[3].set_val(1.75);
+
+   for(int i = 0; i < 1000000; i++) {
+      q[0].set_deriv(0,1.0);
+      q[1].set_deriv(1,1.0);
+      q[2].set_deriv(2,1.0);
+      q[3].set_deriv(3,1.0);
+      ubar = nx*q[1]/q[0]+ny*q[2]/q[0];
+      u = q[1]/q[0];
+      v = q[2]/q[0];
+      
+      p = (gam - 1.0)*(q[3] - .5*(q[1]*q[1] + q[2]*q[2])/q[0]);
+
+      cs = pow((gam*p)/q[0],.5)+pow((gam*p)/q[0],.5);
+
+      f[0] = l*0.25*q[0]*cs*(ubar/cs+1.0)*(ubar/cs+1.0);
+      f[1] = f[0]*((nx/gam)*(-1.0*ubar)+(nx/gam)*(2.0*cs)+u);
+      f[2] = f[0]*((ny/gam)*(-1.0*ubar)+(ny/gam)*(2.0*cs)+v);
+      f[3] = f[0]*((-(gam-1.0)*ubar*ubar+2.0*(gam-1.0)*ubar*cs+2.0*cs*cs)
+            /(gam*gam-1.0)+0.5*(u*u)+0.5*(v*v));
+
+
+
+   }
+   for(int j=0; j <= 3; j++) {
+      cout << "j   " << f[j].Value() << endl;
+      for(int k=0; k <= 3; k++)
+         cout << "j,k   " << f[j].Deriv(k) << endl;
+   }
+
+   return 0;
+}
